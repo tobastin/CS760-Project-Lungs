20d81114c0d677cde1eed805f990b131b13143dc
tobastin <tobastin@gmail.com>
2020-04-08 12:04:54 -0500

    Regression Model
    Added code for CNN regression model for 1 parameter prediction

--- code/master/src/genmodel.py
+++ code/master/src/genmodel.py
@@ -1,8 +1,8 @@
 from keras.layers import *
 from keras.models import Model
 
-def genmodel(input_shape):
-    
+def genmodel_seg(input_shape):
+
     input_layer = Input(shape=input_shape)
     c1 = Conv2D(filters=8, kernel_size=(3,3), activation='relu', padding='same')(input_layer)
     l = MaxPool2D(strides=(2,2))(c1)
@@ -20,5 +20,24 @@ def genmodel(input_shape):
     l = Conv2D(filters=64, kernel_size=(1,1), activation='relu')(l)
     l = Dropout(0.5)(l)
     output_layer = Conv2D(filters=1, kernel_size=(1,1), activation='sigmoid')(l)
-                                                         
+
+    return Model(input_layer, output_layer)
+
+
+
+def genmodel_reg(input_shape):
+
+    input_layer = Input(shape=input_shape)
+    l = Conv2D(filters=8, kernel_size=(3,3), activation='relu', padding='same')(input_layer)    #32x32 -> 32x32x8
+    l = MaxPool2D(strides=(2,2))(l)    #32x32x8 -> 16x16x8
+    l = Conv2D(filters=16, kernel_size=(3,3), activation='relu', padding='same')(l) #16x16x8 -> 16x16x16
+    l = MaxPool2D(strides=(2,2))(l)     # 16x16x16 -> 8x8x16
+    l = Conv2D(filters=16, kernel_size=(3,3), activation='relu', padding='same')(l) # 8x8x16 -> 8x8x16
+    l = MaxPool2D(strides=(2,2))(l)     #8x8x16 -> 4x4x16
+    l = Flatten()(l)
+    l = Dense(units = 256, activation = "relu")(l)
+    l = Dense(units = 128, activation = "relu")(l)
+    l = Dense(units = 16, activation = "relu")(l)
+    output_layer = Dense(units = 1)(l)
+
     return Model(input_layer, output_layer)
--- code/master/src/main_lungs.py
+++ code/master/src/main_lungs.py
@@ -20,77 +20,104 @@ from genmodel import *
 from getdata import *
 from utils import *
 
-IMAGE_LIB = '../input/2d_images/'
-MASK_LIB = '../input/2d_masks/'
-IMG_HEIGHT, IMG_WIDTH = 32, 32
-TEST_RATIO = 0.8
+def main_seg():
+    IMAGE_LIB = '../input/2d_images/'
+    MASK_LIB = '../input/2d_masks/'
+    IMG_HEIGHT, IMG_WIDTH = 32, 32
+    TEST_RATIO = 0.2
+
+    write_seg = True
+
+    # get train/test data
+    x_train, x_val, y_train, y_val = getdata_seg(IMAGE_LIB, MASK_LIB, IMG_HEIGHT, IMG_WIDTH, TEST_RATIO)
+
+    # get model
+    model = genmodel_seg(x_train.shape[1:])
+    #model.summary()
+
+    # train model
+    model.compile(optimizer=Adam(2e-4), loss='binary_crossentropy', metrics=[dice_coef])
+
+    # train setup
+    weight_saver = ModelCheckpoint('lung_seg.h5', monitor='val_dice_coef', save_best_only=True, save_weights_only=True)
+    annealer = LearningRateScheduler(lambda x: 1e-3 * 0.8 ** x)
+
+    # train
+    hist = model.fit_generator(my_generator(x_train, y_train, 8),
+                               steps_per_epoch = 200,
+                               validation_data = (x_val, y_val),
+                               epochs=10, verbose=2,
+                               callbacks = [weight_saver, annealer])
+
+    # model train summary
+    plt.plot(hist.history['loss'], color='b')
+    plt.plot(hist.history['val_loss'], color='r')
+    plt.show()
+    plt.plot(hist.history['dice_coef'], color='b')
+    plt.plot(hist.history['val_dice_coef'], color='r')
+    plt.show()
+
+    # testing
+    model.load_weights('lung_seg.h5')
+    #plt.imshow(model.predict(x_train[10].reshape(1,IMG_HEIGHT, IMG_WIDTH, 1))[0,:,:,0], cmap='gray')
+
+    # test results
+    y_hat = model.predict(x_val)
+    fig, ax = plt.subplots(1,3,figsize=(12,6))
+    ax[0].imshow(x_val[0,:,:,0], cmap='gray')
+    ax[1].imshow(y_val[0,:,:,0])
+    ax[2].imshow(y_hat[0,:,:,0])
+    print(len(y_hat),len(y_hat[0]))
+
+    # TODO
+    #if write_seg:
+    #    for i in range(len(y_hat)):
+    #        imwrite('segmented_image.png', y_hat)
+
+def main_reg():
+    MASK_LIB = '../input/2d_masks/'
+    IMG_HEIGHT, IMG_WIDTH = 32, 32
+    TEST_RATIO = 0.2
+
+    # get train/test data
+    x_train, x_val, y_train, y_val = getdata_reg(MASK_LIB, IMG_HEIGHT, IMG_WIDTH, TEST_RATIO)
+
+    # get model
+    model = genmodel_reg(x_train.shape[1:])
+    #model.summary()
+
+    # train model
+    model.compile(optimizer=Adam(2e-4), loss='mean_squared_error')#, metrics=[dice_coef])
+
+    # train setup
+    weight_saver = ModelCheckpoint('lung_reg.h5', save_best_only=True, save_weights_only=True)
+    #annealer = LearningRateScheduler(lambda x: 1e-3 * 0.8 ** x)
+
+    # train
+    hist = model.fit(x_train, y_train, batch_size=1, epochs=200, validation_data = (x_val, y_val),
+                        verbose=2, callbacks=[weight_saver])#, annealer])
+
+    # model train summary
+    plt.plot(hist.history['loss'], color='b')
+    plt.plot(hist.history['val_loss'], color='r')
+    plt.show()
+
+    # testing
+    model.load_weights('lung_reg.h5')
+    #plt.imshow(model.predict(x_train[10].reshape(1,IMG_HEIGHT, IMG_WIDTH, 1))[0,:,:,0], cmap='gray')
+
+    # test results
+    y_hat = model.predict(x_val)
+    e = [abs(y_val[i]-y_hat[i])*100/y_val[i] for i in range(len(y_val))]
+
+    print("\nAverage % error : ",sum(e)/len(e))
+    #plt.plot(hist.history['loss'], color='b')
+    #plt.plot(hist.history['val_loss'], color='r')
+    #plt.show()
+
+    #print(y_hat)
+    #print(y_val)
 
 
-# get train/test data
-x_train, x_val, y_train, y_val = getdata(IMAGE_LIB, MASK_LIB, IMG_HEIGHT, IMG_WIDTH, TEST_RATIO)
-
-# get model
-model = genmodel(x_train.shape[1:])
-#model.summary()
-
-
-
-# train model
-
-model.compile(optimizer=Adam(2e-4), loss='binary_crossentropy', metrics=[dice_coef])
-
-# train setup
-weight_saver = ModelCheckpoint('lung.h5', monitor='val_dice_coef', 
-                                              save_best_only=True, save_weights_only=True)
-annealer = LearningRateScheduler(lambda x: 1e-3 * 0.8 ** x)
-
-# train
-hist = model.fit_generator(my_generator(x_train, y_train, 8),
-                           steps_per_epoch = 200,
-                           validation_data = (x_val, y_val),
-                           epochs=10, verbose=2,
-                           callbacks = [weight_saver, annealer])
-
-
-# model train summary
-plt.plot(hist.history['loss'], color='b')
-plt.plot(hist.history['val_loss'], color='r')
-plt.show()
-plt.plot(hist.history['dice_coef'], color='b')
-plt.plot(hist.history['val_dice_coef'], color='r')
-plt.show()
-
-
-
-# testing
-
-model.load_weights('lung.h5')
-
-#plt.imshow(model.predict(x_train[10].reshape(1,IMG_HEIGHT, IMG_WIDTH, 1))[0,:,:,0], cmap='gray')
-
-# test results
-
-y_hat = model.predict(x_val)
-fig, ax = plt.subplots(1,3,figsize=(12,6))
-ax[0].imshow(x_val[0,:,:,0], cmap='gray')
-ax[1].imshow(y_val[0,:,:,0])
-ax[2].imshow(y_hat[0,:,:,0])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#main_seg()
+main_reg()
