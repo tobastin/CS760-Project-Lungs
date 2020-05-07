fcff6844e553ace4c111d6cf28e1e1f99514abf4
Apurbaa Bhattacharjee <apurbaa7@gmail.com>
2020-05-06 21:30:08 -0500

    Segmentation visualization added

--- code/master_covid19/src/getdata.py
+++ code/master_covid19/src/getdata.py
@@ -7,32 +7,53 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 def getdata_seg(IMAGE_LIB, MASK_LIB, IMG_HEIGHT, IMG_WIDTH, TEST_RATIO):
+    visualize_idx = [0, 100, 200, 300, 400]
     all_images = [x for x in sorted(os.listdir(IMAGE_LIB)) if x[-4:] == '.png']
 
-    x_data = np.empty((len(all_images), IMG_HEIGHT, IMG_WIDTH), dtype='float32')
+    visualize_x_data = np.empty((len(visualize_idx), IMG_HEIGHT, IMG_WIDTH), dtype='float32')
+    visualize_y_data = np.empty((len(visualize_idx), IMG_HEIGHT, IMG_WIDTH), dtype='float32')
+
+    x_data = np.empty((len(all_images)-len(visualize_idx), IMG_HEIGHT, IMG_WIDTH), dtype='float32')
+    count = 0
+    count_1 = 0
     for i, name in enumerate(all_images):
         im = cv2.imread(IMAGE_LIB + name, 0).astype("int16").astype('float32')
         im = cv2.resize(im, dsize=(IMG_WIDTH, IMG_HEIGHT), interpolation=cv2.INTER_LANCZOS4)
         im = (im - np.min(im)) / (np.max(im) - np.min(im))
-        x_data[i] = im
+        if i in visualize_idx:
+            visualize_x_data[count] = im
+            count = count+1
+        else:
+            x_data[count_1] = im
+            count_1 = count_1+1
 
-    y_data = np.empty((len(all_images), IMG_HEIGHT, IMG_WIDTH), dtype='float32')
+    y_data = np.empty((len(all_images)-len(visualize_idx), IMG_HEIGHT, IMG_WIDTH), dtype='float32')
+    count = 0
+    count_1 = 0
     for i, name in enumerate(all_images):
         im = cv2.imread(MASK_LIB + name, 0).astype('float32')/255.
         im = cv2.resize(im, dsize=(IMG_WIDTH, IMG_HEIGHT), interpolation=cv2.INTER_NEAREST)
-        y_data[i] = im
-
+        if i in visualize_idx:
+            visualize_y_data[count] = im
+            count = count+1
+        else:
+            y_data[count_1] = im
+            count_1 = count_1+1
+    #print(visualize_x_data.shape)
+    #print(visualize_y_data.shape)
+    #assert(0)
     # %% [code]
     #fig, ax = plt.subplots(1,2, figsize = (8,4))
     #ax[0].imshow(x_data[0], cmap='gray')
     #ax[1].imshow(y_data[0], cmap='gray')
 	#plt.show()
 
-    # %% [code]
     x_data = x_data[:,:,:,np.newaxis]
     y_data = y_data[:,:,:,np.newaxis]
-    #x_train, x_val, y_train, y_val = train_test_split(x_data, y_data, test_size = 0.5)
-    return train_test_split(x_data, y_data, test_size = TEST_RATIO)
+    visualize_x_data = visualize_x_data[:,:,:,np.newaxis]
+    visualize_y_data = visualize_y_data[:,:,:,np.newaxis]
+    x_train, x_val, y_train, y_val = train_test_split(x_data, y_data, test_size = TEST_RATIO)
+    return x_train, x_val, y_train, y_val, visualize_x_data, visualize_y_data
 
 def getdata_classification(MASK_LIB, IMG_HEIGHT, IMG_WIDTH, TEST_RATIO):
     with open('../input/classification_gt.csv') as csvfile:
